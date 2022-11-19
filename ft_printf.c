0a2881befc62080216fe05ba7d2a9709ee9c4a2a
Aurelien <aurelien.levra@gmail.com>
2022-11-19 20:54:34 +0100

    3) ca marche pour %d, mais ca affiche pas ce qu'il y a apres le %d PTDR

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 16:08:31 by alevra            #+#    #+#             */
-/*   Updated: 2022/11/19 19:01:50 by alevra           ###   ########lyon.fr   */
+/*   Updated: 2022/11/19 20:52:21 by alevra           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,11 +50,15 @@ char	*ft_strnew(size_t size)
 char	*get_next_str(char *str_src, char c, char *type)
 {
 	int count;
-
+	static int already_read = 0;
 	count = 0;
 	//char *s = ft_strchr(str,c)
 	char *str = ft_strchr(str_src,c);
+	if (!str || !*str)
+		return (NULL);
+	str += already_read;
 	int size = str - str_src;
+	already_read = size;
 	*type = str_src[size + 1];
 	char *new_str = ft_strnew(size);
 	new_str = ft_strncpy(new_str, (const char *) str_src, size);
@@ -73,7 +77,7 @@ void ft_printf(char *str, ...)
 	char *next_str = get_next_str(str,'%', &type);
 	
 	va_start(args, str);
-	while (next_str)
+	while (next_str && next_str[0])
 	{
 		ft_putstr(next_str);
 		if (type == 'd')
@@ -81,13 +85,13 @@ void ft_printf(char *str, ...)
 			int x = va_arg(args, int);
 			ft_putstr(ft_itoa(x));
 		}
-		next_str = get_next_str(str,'%', &type);
+		next_str = get_next_str(next_str,'%', &type);
 	}
 	va_end(args);
 }
 
 int main(int argc, char const *argv[])
-{
-	ft_printf("Bonjour, mon numero est le : %d\n", 4);
+{ 
+	ft_printf("Bonjour, mon numero est le : %dhihihi", 4);
 	return 0;
 }
