331a7a4e15158dca5b45ce9aa01bf5bff8f2ea57
aLeuleu <aurelien.levra@gmail.com>
2022-11-25 13:56:22 +0100

    _

---
+++ main.c
@@ -0,0 +1,22 @@
+#include "ft_printf.h"
+#include "ft_printf.c"
+#include <stdio.h>
+#include "libft/ft_itoa_printf.c"
+#include "libft/ft_put_u_nbr.c"
+#include "libft/ft_putchar.c"
+#include "libft/ft_putnbr_base.c"
+#include "libft/ft_putstr.c"
+#include "libft/ft_strlen.c"
+#include "libft/ft_strchr.c"
+
+int main(int argc, char const *argv[])
+{
+
+	printf("ft_printf :\n");
+	int a = ft_printf("%");
+	printf("\na : %d \n", a);
+	printf("printf :\n");
+	a = printf("%");
+	printf("\na : %d \n", a);
+	return 0;
+}
