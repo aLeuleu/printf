3968c846c81fc83b970ef40de6434cf872bd7251
aLeuleu <aurelien.levra@gmail.com>
2022-11-25 08:44:35 +0100

    ajout de la libft augmentée

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 16:08:31 by alevra            #+#    #+#             */
-/*   Updated: 2022/11/24 16:21:50 by alevra           ###   ########lyon.fr   */
+/*   Updated: 2022/11/25 08:43:08 by alevra           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,6 @@
 # define FT_PRINTF_H
 
 # include <stdarg.h>
-# include <unistd.h>
-# include <stdlib.h>
-
-void	ft_putchar(char c);
-int		ft_put_u_nbr(unsigned int n);
-int		ft_itoa_printf(int n);
-size_t	ft_strlen(const char *s);
-int		ft_putnbr_base(unsigned long nbr, char *base);
-int		ft_putstr(char *str);
+# include "libft.h"
 
 #endif
