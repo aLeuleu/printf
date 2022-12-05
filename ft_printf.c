d919e27fe30fa7640ea71fda2b19ad0d43109559
aLeuleu <aurelien.levra@gmail.com>
2022-12-05 13:58:41 +0100

    _

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 16:08:31 by alevra            #+#    #+#             */
-/*   Updated: 2022/11/30 22:04:43 by alevra           ###   ########lyon.fr   */
+/*   Updated: 2022/12/05 13:39:14 by alevra           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,23 @@
 static int	switch_printf(char format, va_list args)
 {
 	if (format == 'c')
-		return (ft_putchar((char)va_arg(args, int)), 1);
+		return ((int)ft_putchar((char)va_arg(args, int)));
 	if (format == 's')
 		return (ft_putstr(va_arg(args, char *)));
 	if (format == 'p')
 	{
 		return (ft_putstr("0x")
-			+ ft_putnbr_base(va_arg(args, unsigned long), BASE_HEXA));
+			+ ft_put_u_nbr_base(va_arg(args, unsigned long), BASE_HEXA));
 	}
 	if (format == 'd' || format == 'i')
-		return (ft_putnbr(va_arg(args, int))); //ne pas faire de malloc .. ou alors gerer les cas ou le malloc pete
+		return (ft_itoa_printf(va_arg(args, int)));
 	if (format == 'u')
 		return (ft_put_u_nbr(va_arg(args, unsigned int)));
 	if (format == 'x')
-		return (ft_putnbr_base(va_arg(args, unsigned long),
+		return (ft_putnbr_base(va_arg(args, long long),
 				BASE_HEXA));
 	if (format == 'X')
-		return (ft_putnbr_base(va_arg(args, unsigned long), BASE_HEXA_MAJ));
+		return (ft_putnbr_base(va_arg(args, long long), BASE_HEXA_MAJ));
 	if (format == '%')
 		return (ft_putchar('%'), 1);
 	return (0);
@@ -44,26 +44,23 @@ int	ft_printf(const char *str, ...)
 	va_list	args;
 	int		i;
 	int		len;
+	int		res;
 
 	if (write(1, 0, 0) < 0)
 		return (-1);
 	va_start(args, str);
-	i = 0;
+	i = -1;
 	len = 0;
-	while (str[i])
+	while (str[++i])
 	{
 		if (str[i] == '%' && str[i + 1]
 			&& ft_strchr("cspdiuxX%", (int)str[i + 1]))
-		{
-			len += switch_printf(str[i + 1], args);
-			i++;
-		}
+			res = switch_printf(str[i++ + 1], args);
 		else if (str[i] != '%')
-		{
-			ft_putchar(str[i]);
-			len++;
-		}
-		i++;
+			res = ft_putchar(str[i]);
+		if (res < 0)
+			return (-1);
+		len += res;
 	}
 	return (va_end(args), len);
 }
--- main.c
+++ main.c
@@ -1,23 +1,15 @@
 #include "ft_printf.h"
-#include "ft_printf.c"
+#include <limits.h>
 #include <stdio.h>
-#include "libft/ft_itoa_printf.c"
-#include "libft/ft_put_u_nbr.c"
-#include "libft/ft_putchar.c"
-#include "libft/ft_putnbr_base.c"
-#include "libft/ft_putstr.c"
-#include "libft/ft_strlen.c"
-#include "libft/ft_strchr.c"
-#include "libft/ft_putnbr.c"
 
 int main(int argc, char const *argv[])
 {
 
 	printf("ft_printf :\n");
-	int a = ft_printf("%d", -12);
+	int a = ft_printf("%x", LONG_MAX);
 	printf("\na : %d \n", a);
 	printf("printf :\n");
-	a = printf("%d", -12);
+	a = printf("%lx", LONG_MAX);
 	printf("\na : %d \n", a);
 	return 0;
 }
